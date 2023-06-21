547ea370c0cc93d3e0f92f2f871ca8dc22502724
iammohiii <mohade3sh19@gmail.com>
2023-06-21 19:57:24 +0330

    First Commit

---
+++ app/src/main/java/com/example/mohadeseshojaefinalproject/CHARGE.java
@@ -0,0 +1,14 @@
+package com.example.mohadeseshojaefinalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CHARGE extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.charge);
+    }
+}
